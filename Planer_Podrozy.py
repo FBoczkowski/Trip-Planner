62d17d9bff1c8fe62ea5373a927327ce37a2edb2
FBoczkowski <f.boczkowski97@gmail.com>
2023-01-12 19:04:44 +0100

    Add new fields to Class Travel

--- Planer_Podrozy.py
+++ Planer_Podrozy.py
@@ -1,6 +1,11 @@
 class Travel:
-    def __init__(self, place_):
+    def __init__(self, place_, apartment_night_, trip_name_, spend_money_, start_travel_, end_travel_):
         self.place = place_
+        self.aprtment_night = apartment_night_
+        self.trip_name = trip_name_
+        self.spend_money = spend_money_
+        self.start_travel = start_travel_
+        self.end_travel = end_travel_
 
 class City(Travel):
     def __init__(self, city, population_):
@@ -11,3 +16,4 @@ class Country(Travel):
     def __init__(self, country, capitol_):
         super().__init__(country)
         self.capitol = capitol_
+
