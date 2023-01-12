1adf4e5d0dc7029abc38211efc3d628db6c03ce9
FBoczkowski <f.boczkowski97@gmail.com>
2023-01-12 18:47:37 +0100

    Add two new classes City and Country which are Travel subclass

--- Planer_Podrozy.py
+++ Planer_Podrozy.py
@@ -2,3 +2,12 @@ class Travel:
     def __init__(self, place_):
         self.place = place_
 
+class City(Travel):
+    def __init__(self, city, population_):
+        super().__init__(city)
+        self.population = population_
+
+class Country(Travel):
+    def __init__(self, country, capitol_):
+        super().__init__(country)
+        self.capitol = capitol_
