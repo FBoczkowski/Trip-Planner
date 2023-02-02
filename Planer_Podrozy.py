52ef84730a37c550e7cc426ba4d58cdf98d0d766
FBoczkowski <f.boczkowski97@gmail.com>
2023-02-02 17:10:23 +0100

    Add new subclass Transport

--- Planer_Podrozy.py
+++ Planer_Podrozy.py
@@ -6,7 +6,7 @@ class Travel:
         self.spend_money = spend_money_
         self.start_travel = start_travel_
         self.end_travel = end_travel_
-
+    
 class City(Travel):
     def __init__(self, city, population_):
         super().__init__(city)
@@ -16,4 +16,16 @@ class Country(Travel):
     def __init__(self, country, capitol_):
         super().__init__(country)
         self.capitol = capitol_
+
+class Transport(Travel):
+    def __init__(self, travel_cost, transport_type_, travel_time_, travel_A_B_, travel_B_A_):
+        super().__init__(travel_cost)
+        self.transport_type = transport_type_
+        self.travel_time = travel_time_
+        self.travel_A_B = travel_A_B_
+        self.travel_B_A = travel_B_A_
+
+
+
+
 
