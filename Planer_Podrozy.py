0907369624fc03a2cf954246ff802459077bab05
FBoczkowski <f.boczkowski97@gmail.com>
2023-03-09 21:38:23 +0100

    Add new function which import data from wikipedia.

--- Planer_Podrozy.py
+++ Planer_Podrozy.py
@@ -1,27 +1,27 @@
+from typing import List
+
 import wikipedia
 import datetime
 
 class City:
-    def __init__(self, city_, city_information_=0):
-        self.city = city_
-        self.city_information = city_information_
+    def __init__(self, name_, information_=0):
+        self.name = name_
+        self.information =  information_
 
 class Country:
-    def __init__(self, country_, country_information_=0):
-        self.country = country_
-        self.country_information = country_information_
+    def __init__(self, name_, information_=0):
+        self.name = name_
+        self.information = information_
 
 class Travel:
     def __init__(self, trip_name_, spend_money_, start_travel_, finish_travel_, city_: City,
-                 country_: Country, city_info_: City, country_info_: City):
+                 country_: Country):
         self.trip_name = trip_name_
         self.spend_money = spend_money_
         self.start_travel = start_travel_
         self.finish_travel = finish_travel_
-        self.city = city_
-        self.country = country_
-        self.city_info = city_info_
-        self.country_info = country_info_
+        self.city: City = city_
+        self.country: Country = country_
 
 class Transport:
     def __init__(self, travel_time_, travel_A_B_, travel_B_A_):
@@ -31,7 +31,7 @@ class Transport:
 
 class Manager:
     def __init__(self):
-        self.travels = []
+        self.travels: List[Travel] = []
         self.countries = []
         self.cities = []
         self.transports = []
@@ -70,28 +70,25 @@ class Manager:
     def change_date(self, date_str):
         year, month, day = map(int, date_str.split("-"))
         return datetime.date(year, month, day)
+    def wiki_info(self, place):
+        x = wikipedia.summary(f"{place}")
+        return x[:200]
     def add_new_travel(self):
         trip_name = input("Enter Trip name: ")
         country_name = input("Enter country which you visited/planning visit: ")
         city_name = input("Enter city which you visited/planning visit: ")
-        date_start = input("Enter when you started/planning start travel (DD.MM.YYYY): ")
-        date_stop = input("Enter when you finish/planning start travel (DD.MM.YYYY): ")
+        date_start = input("Enter when you started/planning start travel (YYYY.MM.DD): ")
+        date_stop = input("Enter when you finish/planning start travel (YYYY.MM.DD): ")
         spend_money = int(input("Enter how much money you have spent: "))
-
-        info_city = wikipedia.summary(f"{city_name}")
-        info_country = wikipedia.summary(f"{country_name}")
 
         city = City(city_name)
         country = Country(country_name)
-        city_info = City(0, info_city)
-        country_info = Country(0, info_country)
 
         start_travel = self.change_date(date_start)
         finish_travel = self.change_date(date_stop)
 
         new_travel = Travel(trip_name_=trip_name, country_=country, city_=city, start_travel_=start_travel,
-                            finish_travel_=finish_travel, spend_money_=spend_money, city_info_=city_info,
-                            country_info_=country_info)
+                            finish_travel_=finish_travel, spend_money_=spend_money)
 
 
         self.travels.append(new_travel)
@@ -103,12 +100,12 @@ class Manager:
                            "- Car\n"
                            "- Plane\n"
                            "- Bus\n"
-                           "- Train")
+                           "- Train\n")
         travel_B_A = input("Which type of transport do you travel from A to B? You can choose from this list:\n"
                            "- Car\n"
                            "- Plane\n"
                            "- Bus\n"
-                           "- Train")
+                           "- Train\n")
         travel_time = input("How much time do you spend in both way?: ")
 
         new_transport_details = Transport(travel_A_B_=travel_A_B, travel_B_A_=travel_B_A, travel_time_=travel_time)
@@ -120,23 +117,28 @@ class Manager:
                   f"Type of transport, you travel from A to B - {nations.travel_A_B}\n"
                   f"Type of transport, you travel from B to A - {nations.travel_B_A}\n"
                   f"All time which you spend in both way - {nations.travel_time}h\n")
+
     def show_travels(self):
-        for count, trips in enumerate(self.travels):
+        for count, travel in enumerate(self.travels):
             print(f"{count}:\n"
-                  f"Trip name - {trips.trip_name}\n"
-                  f"Country which you visited/planning visit - {trips.country}\n"
-                  f"City which you visited/planning visit - {trips.city}\n"
-                  f"Date when you will start or started travel - {trips.start_travel}\n"
-                  f"Date when you will finish or finished travel - {trips.finish_travel}\n"
-                  f"How much money you spent or will you spend - {trips.spend_money}zł\n")
+                  f"Trip name - {travel.trip_name}\n"
+                  f"Country which you visited/planning visit - {travel.country.name}\n"
+                  f"City which you visited/planning visit - {travel.city.name}\n"
+                  f"Date when you will start or started travel - {travel.start_travel}\n"
+                  f"Date when you will finish or finished travel - {travel.finish_travel}\n"
+                  f"How much money you spent or will you spend - {travel.spend_money}zł\n")
             print("\n")
     def show_cities(self):
-        for count, cities in enumerate(self.cities):
-            print(f"{count} - {cities.city}")
+        for count, city in enumerate(self.cities):
+            print(f"{count}:\n"
+                  f"City name: {city.name}\n"
+                  f"City information: {self.wiki_info(city.name)}...")
         print("\n")
     def show_countries(self):
-        for count, nations in enumerate(self.countries):
-            print(f"{count} - {nations.country}")
+        for count, country in enumerate(self.countries):
+            print(f"{count}:"
+                  f"Country name: {country.name}\n"
+                  f"Country information: {self.wiki_info(country.name)}...")
         print("\n")
 
     def stop_menu(self):
